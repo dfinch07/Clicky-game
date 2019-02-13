fa689075acdf4b559cfbdaec1232578d27f951a2
DVictory <derrickfinch35@gmail.com>
2019-02-12 20:12:24 -0500

    Updates

---
+++ precache-manifest.07b4b65da68ac2fd87b2b1cf356ff7ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "057d0eca7d6ab44d5ee9",
+    "url": "/Clicky-game/static/js/runtime~main.057d0eca.js"
+  },
+  {
+    "revision": "f727bb3758722740d96c",
+    "url": "/Clicky-game/static/js/main.f727bb37.chunk.js"
+  },
+  {
+    "revision": "c4e87af5680bcfba6d14",
+    "url": "/Clicky-game/static/js/2.c4e87af5.chunk.js"
+  },
+  {
+    "revision": "f727bb3758722740d96c",
+    "url": "/Clicky-game/static/css/main.ba890334.chunk.css"
+  },
+  {
+    "revision": "fe9f17dae08af77e6ca0a91e9cd56716",
+    "url": "/Clicky-game/index.html"
+  }
+];
